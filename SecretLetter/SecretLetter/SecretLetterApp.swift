cf2d6f7a0d1b3e2c2733cc71c1f7790d23ef6f81
Daegun Choi  <chcs1370@gmail.com>
2024-04-06 19:34:46 +0900

    테스트 코드 원복

--- SecretLetter/SecretLetter/SecretLetterApp.swift
+++ SecretLetter/SecretLetter/SecretLetterApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SecretLetterApp: App {
     var body: some Scene {
         WindowGroup {
-            MakeLetterView(viewModel: MakeLetterViewModel(senderName: "Unithon"))
+            SplashView()
         }
     }
 }
