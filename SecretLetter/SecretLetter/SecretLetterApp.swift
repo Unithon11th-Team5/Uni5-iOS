fe1715845c423a3e862d54afb97e14370920a31e
Daegun Choi  <chcs1370@gmail.com>
2024-04-06 11:05:54 +0900

    Create Project

---
+++ SecretLetter/SecretLetter/SecretLetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecretLetterApp.swift
+//  SecretLetter
+//
+//  Created by Daegeon Choi on 4/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SecretLetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
