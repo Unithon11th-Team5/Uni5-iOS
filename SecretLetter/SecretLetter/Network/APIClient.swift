e51de946b9a5826b911bf41b75d2693f2db75ed2
Daegun Choi  <chcs1370@gmail.com>
2024-04-06 13:47:26 +0900

    Alamofire 설치, APIClient 생성

---
+++ SecretLetter/SecretLetter/Network/APIClient.swift
@@ -0,0 +1,14 @@
+//
+//  APIClient.swift
+//  SecretLetter
+//
+//  Created by Daegeon Choi on 4/6/24.
+//
+
+import Foundation
+
+class APIClient {
+    
+    private let baseURL: String = "example.com"
+    
+}
